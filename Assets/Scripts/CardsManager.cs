03d5c69d63979501c0ba1166ebb1ced5be002a35
Yovador <yovador@gmail.com>
2021-12-30 16:44:54 +0100

    visual base

---
+++ Assets/Scripts/CardsManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Script responsible of the display of the cards.
+// It uses the data stored in "galery.json"
+
+public class CardsManager : MonoBehaviour
+{
+    [SerializeField] private GameObject cardPrefab;
+
+    private void Start()
+    {
+        
+    }
+
+    private void AddACardToView()
+    {
+
+    }
+}
